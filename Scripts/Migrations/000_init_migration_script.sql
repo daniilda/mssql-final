0f6874ff278a2ec614645f08af9cf26170134cc6
DanielK <m2002725@edu.misis.ru>
2021-12-08 00:37:49 +0300

    [DEV-0] init commit

---
+++ Scripts/Migrations/000_init_migration_script.sql
@@ -0,0 +1,114 @@
+USE [master]
+GO
+CREATE DATABASE [CommerceClinic]
+GO
+
+USE [CommerceClinic]
+GO
+
+BEGIN TRANSACTION
+
+CREATE TABLE [Patients]
+(
+    [Id]                 INT PRIMARY KEY IDENTITY,
+    [FirstName]          NVARCHAR(60)  NULL,
+    [SecondName]         NVARCHAR(60)  NULL,
+    [MiddleName]         NVARCHAR(60)  NULL,
+    [Gender]             BIT           NOT NULL,
+    [DateOfBirth]        DATE          NOT NULL,
+    [Address]            NVARCHAR(100) NULL,
+    [PaymentCredentials] NVARCHAR(100) NOT NULL,
+    [InsuranceData]      NVARCHAR(100) NULL
+);
+
+CREATE TABLE [StaffCategory]
+(
+    [Id]          INT PRIMARY KEY IDENTITY,
+    [Name]        NVARCHAR(60)  NOT NULL UNIQUE,
+    [Description] NVARCHAR(100) NULL,
+    [CostRate]    DECIMAL       NOT NULL
+);
+
+CREATE TABLE [MedicalStaff]
+(
+    [Id]         INT PRIMARY KEY IDENTITY,
+    [FirstName]  NVARCHAR(60) NULL,
+    [SecondName] NVARCHAR(60) NULL,
+    [MiddleName] NVARCHAR(60) NULL,
+    [Gender]     BIT          NOT NULL,
+    [CategoryId] INT          NOT NULL,
+    [Degree]     NVARCHAR(60) NULL,
+    [Area]       NVARCHAR(60) NOT NULL,
+    FOREIGN KEY (CategoryId) REFERENCES [StaffCategory] (Id)
+);
+
+CREATE TABLE [Drugs]
+(
+    [Id]          INT PRIMARY KEY IDENTITY,
+    [Name]        NVARCHAR(60)  NOT NULL UNIQUE,
+    [Description] NVARCHAR(100) NULL
+);
+
+CREATE TABLE [Prescriptions]
+(
+    [Id]             INT PRIMARY KEY IDENTITY,
+    [PatientId]      INT           NOT NULL,
+    [MedicalStaffId] INT           NOT NULL,
+    [DrugId]         INT           NOT NULL,
+    [Description]    NVARCHAR(100) NOT NULL,
+    [ExpiresAt]      DATE          NOT NULL,
+    FOREIGN KEY (PatientId) REFERENCES [Patients] (Id),
+    FOREIGN KEY (MedicalStaffId) REFERENCES [MedicalStaff] (Id),
+    FOREIGN KEY (DrugId) REFERENCES [Drugs] (Id)
+);
+
+CREATE TABLE [TimeSlots]
+(
+    [Id]   INT PRIMARY KEY IDENTITY,
+    [From] DATETIME NOT NULL,
+    [To]   DATETIME NOT NULL
+);
+
+CREATE TABLE [Bills]
+(
+    [Id]          INT PRIMARY KEY IDENTITY,
+    [Amount]      DECIMAL       NOT NULL,
+    [PaymentInfo] NVARCHAR(100) NULL
+);
+
+CREATE TABLE [ServiceTypes]
+(
+    [Id]          INT PRIMARY KEY IDENTITY,
+    [Name]        NVARCHAR(60)  NOT NULL UNIQUE,
+    [Description] NVARCHAR(100) NULL
+);
+
+CREATE TABLE [Services]
+(
+    [Id]          INT PRIMARY KEY IDENTITY,
+    [Name]        NVARCHAR(60)  NOT NULL,
+    [Description] NVARCHAR(100) NULL,
+    [Price]       DECIMAL       NOT NULL,
+    [TypeId]      INT           NOT NULL,
+    FOREIGN KEY (TypeId) REFERENCES [ServiceTypes] (Id)
+);
+
+CREATE TABLE [Referrals]
+(
+    [Id]             INT PRIMARY KEY IDENTITY,
+    [PatientId]      INT           NOT NULL,
+    [MedicalStaffId] INT           NOT NULL,
+    [ServiceId]      INT           NOT NULL,
+    [TimeSlotId]     INT           NOT NULL,
+    [Result]         NVARCHAR(100) NOT NULL,
+    [BillId]         INT           NULL,
+    FOREIGN KEY (PatientId) REFERENCES [Patients] (Id),
+    FOREIGN KEY (MedicalStaffId) REFERENCES [MedicalStaff] (Id),
+    FOREIGN KEY (ServiceId) REFERENCES [Services] (Id),
+    FOREIGN KEY (TimeSlotId) REFERENCES [TimeSlots] (Id),
+    FOREIGN KEY (BillId) REFERENCES [Bills] (Id)
+);
+
+COMMIT TRANSACTION;
+
+
